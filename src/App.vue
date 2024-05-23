d2ce61de92db8ef83dab321f9ca2e5af667d946b
yonava <yona@vossandreae.com>
2024-05-23 10:55:34 -0500

    working latexInput component

--- src/App.vue
+++ src/App.vue
@@ -1,161 +1,24 @@
 <script setup lang="ts">
-import 'katex/dist/katex.min.css';
+import LatexInput from './components/LatexInput.vue';
 import { setLatexToExpression, setParser } from './expressionParser';
-import { ref, onMounted, watch, computed } from 'vue';
-import katex from 'katex';
+import { ref, computed } from 'vue';
 
-const tex = ref<HTMLDivElement | null>(null);
-const cursorTex = ref<HTMLDivElement | null>(null);
+const latexInputString = ref('');
 
-const text = ref('')
-const error = ref('');
-
-const cursorPosition = ref(0);
-
-const cursorBoxText = computed(() => {
-  return text.value.slice(0, cursorPosition.value);
-});
-
-const renderTex = () => {
-  try {
-    katex.render(text.value, tex.value);
-    katex.render(cursorBoxText.value, cursorTex.value);
-    error.value = '';
-  } catch (e) {
-    error.value = 'Invalid LaTeX';
-  }
-}
-
-onMounted(() => {
-  if (!tex.value) {
-    return;
-  }
-
-  tex.value.addEventListener('keydown', inputKeyPressHandler);
-
-  tex.value.addEventListener('copy', (event) => {
-    event.preventDefault();
-    text.value = text.value.slice(0, -1);
-    navigator.clipboard.writeText(text.value);
-  });
-
-  tex.value.addEventListener('paste', async (event) => {
-    event.preventDefault();
-    text.value = text.value.slice(0, -1);
-    const t = await navigator.clipboard.readText();
-    text.value += t;
-  });
-
-  tex.value.addEventListener('cut', (event) => {
-    event.preventDefault();
-    text.value = text.value.slice(0, -1);
-    navigator.clipboard.writeText(text.value);
-  });
-
-});
-
-const inputKeyPressHandler = (event: KeyboardEvent) => {
-  event.preventDefault();
-
-  if (event.key === 'ArrowLeft') {
-    const charBeingCursedOver = text.value[cursorPosition.value - 1];
-    if (charBeingCursedOver !== ' ') {
-      cursorPosition.value = Math.max(0, cursorPosition.value - 1);
-      return;
-    }
-    const lastSlash = cursorBoxText.value.lastIndexOf('\\');
-    if (lastSlash === -1) cursorPosition.value = 0;
-    else cursorPosition.value = lastSlash;
-    return;
-  }
-
-  if (event.key === 'ArrowRight') {
-    const charBeingCursedOver = text.value[cursorPosition.value];
-    if (charBeingCursedOver !== '\\') {
-      cursorPosition.value = Math.min(text.value.length, cursorPosition.value + 1);
-      return;
-    } else {
-      const nextSpace = text.value.indexOf(' ', cursorPosition.value);
-      if (nextSpace === -1) cursorPosition.value = text.value.length;
-      else cursorPosition.value = nextSpace + 1;
-    }
-
-    return;
-  }
-
-  if (event.key === 'Backspace') {
-
-    const selectedText = window.getSelection()!.toString();
-
-    if (selectedText) {
-      text.value = text.value.replace(selectedText, '');
-      return;
-    }
-
-    const lastChar = text.value.slice(-1);
-    const stringMinusLastChar = text.value.slice(0, -1);
-
-    // if the char we are removing is a space, it means we hit a latex command
-    if (lastChar === ' ') {
-      const lastSlash = stringMinusLastChar.lastIndexOf('\\');
-      text.value = text.value.slice(0, lastSlash)
-    } else {
-      text.value = stringMinusLastChar;
-    }
-
-    return;
-  }
-
-  if (event.key.length > 1) {
-    return;
-  }
-
-  if (event.key === 'i') {
-    text.value += '\\cap ';
-    cursorPosition.value += 5;
-  } else if (event.key === 'u') {
-    text.value += '\\cup ';
-    cursorPosition.value += 5;
-  } else if (event.key === 'd') {
-    text.value += '\\Delta ';
-    cursorPosition.value += 7;
-  } else {
-    text.value += event.key.toUpperCase();
-  }
-}
-
-const setInputFocus = (state: boolean) => {
-  if (!tex.value) return;
-
-  if (!cursorTex.value) return;
-
-  if (state) {
-    tex.value.classList.add('input-field');
-    tex.value.classList.remove('input-field-inactive');
-  } else {
-    tex.value.classList.remove('input-field');
-    tex.value.classList.add('input-field-inactive');
-  }
-}
-
-watch(text, renderTex);
-
-const parserExpr = computed(() => {
-  return setLatexToExpression(text.value);
-});
+const setSpace = [
+  ['A'],
+  ['A', 'B'],
+  ['B'],
+  ['C'],
+  ['B', 'C']
+];
 
 const output = computed(() => {
-  const parser = setParser([
-    ['A'],
-    ['A', 'B'],
-    ['B'],
-    ['C'],
-    ['B', 'C'],
-    []
-  ]);
+  const expr = setLatexToExpression(latexInputString.value);
+  const parse = setParser(setSpace);
 
   try {
-    return parser(parserExpr.value);
+    return parse(expr);
   } catch (e) {
     return 'Could Not Parse'
   }
@@ -163,88 +26,10 @@ const output = computed(() => {
 </script>
 
 <template>
-  <div style="position: relative">
-    <div
-      tabindex="0"
-      class="input-field-inactive text-box"
-      @focus="setInputFocus(true)"
-      @blur="setInputFocus(false)"
-      ref="tex"
-    ></div>
-  </div>
-
   <div>
-    <span>
-      {{ error }}
-    </span>
+    <LatexInput v-model="latexInputString" />
   </div>
-  <div
-    :style="{
-      width: 400 + 'px',
-      height: 20 + 'px',
-    }"
-
-  >
-    "{{ text }}"
-  </div>
-  <span>
-    {{ cursorPosition }} - "{{ cursorBoxText }}"
-  </span>
-  <br>
-  <br>
   <code>
     {{ JSON.stringify(output, null, 2) }}
   </code>
 </template>
-
-<style>
-canvas {
-  border: 5px solid black;
-}
-
-@keyframes cursor {
-  0% {
-    opacity: 0;
-  }
-  1% {
-    opacity: 1;
-  }
-  50% {
-    opacity: 1;
-  }
-  51% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-
-.text-box {
-  border: 1px solid black;
-  width: 500px;
-  height: 24px;
-  border-radius: 50px;
-  padding: 3px;
-  padding-left: 10px;
-  cursor: text;
-}
-
-.input-field:focus {
-  outline: none;
-}
-
-.input-field-inactive {
-  background: rgb(233, 233, 233);
-}
-
-.input-field::after {
-  content: '';
-  position: absolute;
-  width: 1px;
-  height: 20px;
-  background-color: black;
-  margin-left: 1px;
-  animation: cursor 1s infinite;
-}
-</style>
---
+++ src/components/LatexInput.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+import { ref, watch, onMounted } from 'vue';
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
+
+const latexString = defineModel<string>({ required: true });
+
+const latexInput = ref<HTMLDivElement | null>(null);
+const renderError = ref('');
+
+onMounted(() => {
+  if (!latexInput.value) return;
+
+  latexInput.value.addEventListener('keydown', inputKeyPressHandler);
+
+  latexInput.value.addEventListener('copy', (event) => {
+    event.preventDefault();
+    latexString.value = latexString.value.slice(0, -1);
+    navigator.clipboard.writeText(latexString.value);
+  });
+
+  latexInput.value.addEventListener('paste', async (event) => {
+    event.preventDefault();
+    latexString.value = latexString.value.slice(0, -1);
+    const t = await navigator.clipboard.readText();
+    latexString.value += t;
+  });
+
+  latexInput.value.addEventListener('cut', (event) => {
+    event.preventDefault();
+    latexString.value = latexString.value.slice(0, -1);
+    navigator.clipboard.writeText(latexString.value);
+  });
+
+});
+
+
+const inputKeyPressHandler = (event: KeyboardEvent) => {
+  event.preventDefault();
+
+  if (event.key === 'Backspace') {
+
+    const selectedText = window.getSelection()!.toString();
+
+    if (selectedText) {
+      latexString.value = latexString.value.replace(selectedText, '');
+      return;
+    }
+
+    const lastChar = latexString.value.slice(-1);
+    const stringMinusLastChar = latexString.value.slice(0, -1);
+
+    // if the char we are removing is a space, it means we hit a latex command
+    if (lastChar === ' ') {
+      const lastSlash = stringMinusLastChar.lastIndexOf('\\');
+      latexString.value = latexString.value.slice(0, lastSlash)
+    } else {
+      latexString.value = stringMinusLastChar;
+    }
+
+    return;
+  }
+
+  if (event.key.length > 1) {
+    return;
+  }
+
+  if (event.key === 'i') {
+    latexString.value += '\\cap ';
+  } else if (event.key === 'u') {
+    latexString.value += '\\cup ';
+  } else if (event.key === 'd') {
+    latexString.value += '\\Delta ';
+  } else {
+    latexString.value += event.key.toUpperCase();
+  }
+}
+
+const renderLatexInInput = () => {
+  try {
+    katex.render(latexString.value, latexInput.value);
+    renderError.value = '';
+  } catch (e) {
+    renderError.value = 'Invalid LaTeX';
+  }
+}
+
+const setLatexInputFocus = (state: boolean) => {
+  if (!latexInput.value) return;
+
+  if (state) {
+    latexInput.value.classList.add('input-field');
+    latexInput.value.classList.remove('input-field-inactive');
+  } else {
+    latexInput.value.classList.remove('input-field');
+    latexInput.value.classList.add('input-field-inactive');
+  }
+}
+
+watch(latexString, renderLatexInInput);
+</script>
+
+<template>
+  <div
+    tabindex="0"
+    class="input-field-inactive text-box"
+    @focus="setLatexInputFocus(true)"
+    @blur="setLatexInputFocus(false)"
+    ref="latexInput"
+  ></div>
+  <span>
+    {{ renderError }}
+  </span>
+</template>
+
+<style scoped>
+@keyframes cursor {
+  0% {
+    opacity: 0;
+  }
+  1% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 1;
+  }
+  51% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+.text-box {
+  border: 1px solid black;
+  width: 500px;
+  height: 24px;
+  border-radius: 50px;
+  padding: 3px;
+  padding-left: 10px;
+  cursor: text;
+}
+
+.input-field:focus {
+  outline: none;
+}
+
+.input-field-inactive {
+  background: rgb(233, 233, 233);
+}
+
+.input-field::after {
+  content: '';
+  position: absolute;
+  width: 1px;
+  height: 20px;
+  background-color: black;
+  margin-left: 1px;
+  animation: cursor 1s infinite;
+}
+</style>
