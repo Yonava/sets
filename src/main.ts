6800337184f25778b3dec2c1a0318156728dbe7a
yonava <yonavossandreae@gmail.com>
2025-10-06 11:03:45 -0400

    added math live dep

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import 'katex/dist/katex.min.css';
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import "katex/dist/katex.min.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import "mathlive";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia());
 
-app.mount('#app')
+app.mount("#app");
--- src/sets/components/LatexInput.vue
+++ src/sets/components/LatexInput.vue
@@ -1,145 +1,24 @@
 <script setup lang="ts">
-  import katex from "katex";
-  import { ref, watch, onMounted } from "vue";
-  import { backspace } from "@/sets/other/latexInputBackspace";
+import { onMounted } from 'vue';
+import { ref } from 'vue';
 
-  const props = defineProps<{
-    hotkeys: Record<string, string>;
-    transform: (c: string) => string;
-  }>();
+  const id = 'math-element-to-grab';
 
-  const latexString = defineModel<string>({ required: true });
+  const test = ref<HTMLElement | null>(null)
 
-  const latexInput = ref<HTMLDivElement | null>(null);
-  const renderError = ref("");
 
   onMounted(() => {
-    if (!latexInput.value) return;
-
-    latexInput.value.addEventListener("keydown", inputKeyPressHandler);
-
-    latexInput.value.addEventListener("copy", (event) => {
-      event.preventDefault();
-      latexString.value = latexString.value.slice(0, -1);
-      navigator.clipboard.writeText(latexString.value);
-    });
-
-    latexInput.value.addEventListener("paste", async (event) => {
-      event.preventDefault();
-      latexString.value = latexString.value.slice(0, -1);
-      const t = await navigator.clipboard.readText();
-      latexString.value += t;
-    });
-
-    latexInput.value.addEventListener("cut", (event) => {
-      event.preventDefault();
-      latexString.value = latexString.value.slice(0, -1);
-      navigator.clipboard.writeText(latexString.value);
-    });
-  });
-
-  const inputKeyPressHandler = (event: KeyboardEvent) => {
-    event.preventDefault();
-
-    if (event.key === "Backspace") {
-      return (latexString.value = backspace(latexString.value));
-    }
-
-    if (event.key.length > 1) {
-      return;
-    }
-
-    // hotkeys tied to latex commands
-    if (props.hotkeys[event.key]) {
-      latexString.value += props.hotkeys[event.key];
-      return;
-    }
-
-    latexString.value += props.transform(event.key);
-  };
-
-  const renderLatexInInput = () => {
-    if (!latexInput.value) throw new Error("latex input not mounted");
-    try {
-      katex.render(latexString.value, latexInput.value);
-      renderError.value = "";
-    } catch (e) {
-      renderError.value = "Invalid LaTeX";
-    }
-  };
-
-  const setLatexInputFocus = (state: boolean) => {
-    if (!latexInput.value) return;
-
-    if (state) {
-      latexInput.value.classList.add("input-field");
-      latexInput.value.classList.remove("input-field-inactive");
-      latexInput.value.focus();
-    } else {
-      latexInput.value.classList.remove("input-field");
-      latexInput.value.classList.add("input-field-inactive");
-    }
-  };
-
-  watch(latexString, (newStr, oldStr) => {
-    renderLatexInInput();
-    if (newStr.length > oldStr.length) {
-      setLatexInputFocus(true);
-    }
-  });
+    test.value = document.getElementById(id)
+    console.log(test.value)
+  })
 </script>
 
 <template>
-  <div
-    tabindex="0"
-    class="input-field-inactive text-box"
-    @focus="setLatexInputFocus(true)"
-    @blur="setLatexInputFocus(false)"
-    ref="latexInput"
-  ></div>
+  <math-field :id="id">x=\frac{-b\pm \sqrt{b^2-4ac}}{2a}</math-field>
 </template>
 
 <style scoped>
-  @keyframes cursor {
-    0% {
-      opacity: 0;
-    }
-    1% {
-      opacity: 1;
-    }
-    50% {
-      opacity: 1;
-    }
-    51% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 0;
-    }
-  }
-
-  .text-box {
-    height: 30px;
-    padding: 3px;
-    padding-left: 10px;
-    cursor: text;
-  }
-
-  .input-field:focus {
-    outline: none;
-  }
-
-  .input-field-inactive {
-    background: #e2dcdc;
-  }
-
-  .input-field::after {
-    content: "";
-    position: absolute;
-    width: 1px;
-    height: 20px;
-    background-color: black;
-    margin-left: 1px;
-    animation: cursor 1s infinite;
-  }
+math-field::part(virtual-keyboard-toggle), math-field::part(menu-toggle) {
+  display: none;
+}
 </style>
